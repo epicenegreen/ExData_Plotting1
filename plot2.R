8b21e7fe5cbcc526c7e3561bc7efbc604d9a9d5a
JESSICA JAMES <jajamesdna@yahoo.com>
2015-05-09 16:36:31 -0700

    Create plot2.R

---
+++ plot2.R
@@ -0,0 +1,57 @@
+##  plot2.R
+##  050715 JAJ
+##  https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip
+##  The dataset has 2,075,259 rows and 9 columns. First calculate a rough estimate of how much memory the dataset will require in memory before reading into R. Make sure your computer has enough memory (most modern computers should be fine).
+##  We will only be using data from the dates 2007-02-01 and 2007-02-02. One alternative is to read the data from just those dates rather than reading in the entire dataset and subsetting to those dates.
+##  You may find it useful to convert the Date and Time variables to Date/Time classes in R using the strptime() and as.Date() functions.
+##  Note that in this dataset missing values are coded as ?.
+## 2,075,259    rows
+##         9	columns
+##         8	bytes/numeric
+## 149418648	bytes
+## 142.496727	MB
+## 0.142496727	GB
+
+reload <- FALSE   ## performance, FALSE to not download data file repeatedly
+destZipFile <- ".\\data\\household_power_consumption.zip"  ## name of local data file
+destOutputFile <- ".\\data\\tidy_household_power_consumption.txt"  ## name of local data file
+##  Define data file
+fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+
+##  Find/create data directory
+if (!file.exists("data")) { dir.create("data") }
+
+## Find/load local data file and only download file if reload is TRUE
+if ((!file.exists(destZipFile)) || (reload = TRUE)) {
+    ##  Note: since URL may be https, and dev of script is on Windows, use curl
+    download.file(url = fileUrl, destfile = destZipFile, method = "auto")
+    ##  Save date of download for reference
+    dateDownloaded <- date()
+    #unzip(localFile, overwrite=TRUE, exdir="data")
+    unzip(destZipFile, overwrite=TRUE, exdir="data")
+}
+
+dataFile <- "data/household_power_consumption.txt"
+
+initialData <- read.table("./data/household_power_consumption.txt", sep=";", header=TRUE, 
+                           stringsAsFactors=FALSE, row.names=NULL, na.strings="?", nrows=50)
+classes <- sapply(initialData, class)
+powerData <- read.table("./data/household_power_consumption.txt", sep=";", header=TRUE, 
+                        stringsAsFactors=FALSE, row.names=NULL, na.strings="?",
+                        comment.char="",
+                        colClasses=classes)
+
+## Dates read 16/12/2006 as.Date(x, %e/%m/%Y) becomes 2006-12-16
+## Times as 17:24:00 strptime(x, "%H:%M:%S")
+# create new data frame of just the two days of interest, returns 2880 rows of 9 variables
+DFdays <- powerData[(as.Date(powerData$Date, "%e/%m/%Y")) == "2007-02-01" | (as.Date(powerData$Date, "%e/%m/%Y")) == "2007-02-02",]
+
+paste(DFdays$Date, DFdays$Time)
+
+## Create 2nd png file
+png(file = "plot2.png", width = 480, height = 480)
+with(DFdays, plot(strptime((paste(DFdays$Date, DFdays$Time)), format="%d/%m/%Y %H:%M:%S"),
+                  DFdays$Global_active_power, xlab = "",
+                  ylab = "Global Active Power (kilowatts)", col = "black",
+                  main= "", axes = TRUE, type="l"))
+dev.off()
